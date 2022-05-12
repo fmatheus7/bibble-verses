3294e5747ef3a3086dd8441e18c48fb996556752
Matheus Freire <matheusfreire77@gmail.com>
2022-05-12 11:36:09 +0100

    teste

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>
+          teste
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
